07a6b32bcfcba26ccf5dedccb79d978e4dcdfabf
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-11-15 12:26:56 +0500

    INFRA: Add Design System Section.

---
+++ lib/core/design_system/app_colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // LIGHT THEME COLORS
+  final Color backgroundLight = Color(0xFFffffff);
+  final Color primaryLight = Color(0xFF030213);
+  final Color cardLight = Color(0xFFffffff);
+  final Color mutedForegroundLight = Color(0xff707081);
+  final Color accentLight = Color(0xFFe9ebef);
+  final Color borderLight = Color.fromRGBO(220, 220, 222, 1);
+
+  // DARK THEME COLORS
+  final Color backgroundDark = Color(0xFF0f1419);
+  final Color primaryDark = Color(0xFFe6e8eb);
+  final Color cardDark = Color(0xFF1a1f25);
+  final Color mutedForegroundDark = Color(0xFF9ca3af);
+  final Color accentDark = Color(0xFF374151);
+  final Color borderDark = Color(0xFF374151);
+
+  // COMMON COLORS
+  final Color white = Color(0xFFffffff);
+  final Color greenPrimary = Color(0xff22c55e);
+  final Color greenLight = Color(0xFF16a34a);
+  final Color softGreen = Color(0xff7AF2A8);
+  final Color redError = Color(0xFFd4183d);
+  final Color orangeWarning = Color(0xFFf97316);
+  final Color yellowPending = Color(0xffeab308);
+  final Color purple = Color(0xffAD47FF);
+  final Color blue = Color(0xff2B7FFF);
+  final Color softBlue = Color(0xff8EC6FF);
+  final Color cardBg = Color(0xff1A1F25); 
+  final Color transparent = Colors.transparent;
+}
