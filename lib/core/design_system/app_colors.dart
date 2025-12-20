5f91acf3827ea9c3fb8a82cf05fbfb3f6196d4d9
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-12-20 15:59:43 +0500

    INFRA: Add Necessary Colors.

--- lib/core/design_system/app_colors.dart
+++ lib/core/design_system/app_colors.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // LIGHT THEME COLORS
-  final Color backgroundLight = Color(0xFFffffff);
-  final Color primaryLight = Color(0xFF030213);
-  final Color cardLight = Color(0xFFffffff);
-  final Color mutedForegroundLight = Color(0xff707081);
-  final Color accentLight = Color(0xFFe9ebef);
-  final Color borderLight = Color.fromRGBO(220, 220, 222, 1);
-
   // DARK THEME COLORS
-  final Color backgroundDark = Color(0xFF0f1419);
-  final Color primaryDark = Color(0xFFe6e8eb);
-  final Color cardDark = Color(0xFF1a1f25);
-  final Color mutedForegroundDark = Color(0xFF9ca3af);
-  final Color accentDark = Color(0xFF374151);
-  final Color borderDark = Color(0xFF374151);
+  final Color backgroundDark = Color(0xFF0E172B);
+  final Color primaryDark = Color(0xFFFFFFFF);
+  final Color secondaryDark = Color(0xFF60738C);
+  final Color cardDark = Color(0xFF161F35);
+  final Color accentDark = Color(0xFF324157);
+  final Color borderDark = Color(0xFF222B3F);
 
   // COMMON COLORS
   final Color white = Color(0xFFffffff);
@@ -25,9 +17,11 @@ class AppColors {
   final Color redError = Color(0xFFd4183d);
   final Color orangeWarning = Color(0xFFf97316);
   final Color yellowPending = Color(0xffeab308);
-  final Color purple = Color(0xffAD47FF);
+  final Color purple = Color(0xFF8A5CF5);
   final Color blue = Color(0xff2B7FFF);
   final Color softBlue = Color(0xff8EC6FF);
   final Color cardBg = Color(0xff1A1F25); 
   final Color transparent = Colors.transparent;
+
+  
 }
