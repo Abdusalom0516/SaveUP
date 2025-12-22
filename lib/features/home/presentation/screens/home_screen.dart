ada28f00619f8f5e2bac12f22d41c1fe1ec2395f
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-12-22 13:41:25 +0500

    BASE: Create App Bar Section In Home Screen.

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -2,9 +2,10 @@ import 'package:boilerplate/core/constants/const_img_paths.dart';
 import 'package:boilerplate/core/design_system/app_text_styles.dart';
 import 'package:boilerplate/core/utils/app_state_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
@@ -48,6 +49,51 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+
+          // Body Section
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16.r),
+            sliver: SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(16.r),
+                    decoration: BoxDecoration(
+                      color: colorScheme.primaryContainer,
+                      border: Border.all(
+                        color: colorScheme.outline,
+                        width: 1.3.r,
+                      ),
+                      borderRadius: BorderRadius.circular(12.r),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              texts.overview,
+                              style: AppTextStyles.roboto.medium(
+                                fontSize: 16.sp,
+                                color: colorScheme.primary,
+                              ),
+                            ),
+                            Text(
+                              "2 ${texts.goalsLower} • \$450 ${texts.savedLower}",
+                              style: AppTextStyles.roboto.medium(
+                                fontSize: 14.sp,
+                                color: colorScheme.secondary,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
