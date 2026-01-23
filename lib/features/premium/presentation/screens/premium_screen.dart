89d4ab09f8cf5d257b01c432e674bdc1580f18d6
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-23 16:09:45 +0500

    BASE: Finish Why Upgrade Section.

--- lib/features/premium/presentation/screens/premium_screen.dart
+++ lib/features/premium/presentation/screens/premium_screen.dart
@@ -24,6 +24,62 @@ class PremiumScreen extends StatelessWidget {
             SliverHeight(height: 20),
             // Plans Section
             _buildPlanCardsSection(colors),
+            SliverHeight(height: 20),
+            // Why Upgrade Section
+            SliverPadding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 16.r),
+              sliver: SliverToBoxAdapter(
+                child: Container(
+                  padding: EdgeInsets.all(16.r),
+                  decoration: BoxDecoration(
+                    color: colorScheme.tertiary.withValues(alpha: 0.3),
+                    borderRadius: BorderRadius.circular(12.r),
+                    border: Border.all(
+                      color: colorScheme.outline,
+                      width: 1.5.r,
+                    ),
+                  ),
+                  child: Column(
+                    spacing: 15.h,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Why Upgrade?",
+                        style: AppTextStyles.roboto.medium(
+                          fontSize: 18.sp,
+                          color: colorScheme.primary,
+                        ),
+                      ),
+                      Column(
+                        spacing: 10.h,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          WhyUpgradeCard(
+                            icon: Icons.stacked_line_chart,
+                            mainColor: colors.purple,
+                            title: "Track More Goals",
+                            subTitle: "Save for multiple dreams simultaneously",
+                          ),
+                          WhyUpgradeCard(
+                            icon: Icons.dark_mode_sharp,
+                            mainColor: colors.blue,
+                            title: "Stay Motivated",
+                            subTitle:
+                                "Visual progress tracking keeps you on track",
+                          ),
+                          WhyUpgradeCard(
+                            icon: Icons.filter_vintage_sharp,
+                            mainColor: colors.orange,
+                            title: "Lifetime Access",
+                            subTitle: "One-time payment, no subscriptions",
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             SliverHeight(height: 35),
           ],
         ),
@@ -33,67 +89,67 @@ class PremiumScreen extends StatelessWidget {
 
   SliverPadding _buildPlanCardsSection(AppColors colors) {
     return SliverPadding(
-            padding: EdgeInsetsGeometry.symmetric(horizontal: 16.r),
-            sliver: SliverToBoxAdapter(
-              child: Column(
-                spacing: 15.h,
-                children: [
-                  PlanCard(
-                    planTitle: "Starter",
-                    icon: Icons.webhook_outlined,
-                    func: () {},
-                    buttonText: "Get Starter",
-                    dreamCount: "1",
-                    mainColor: colors.blue,
-                    price: "2.99",
-                    priceCategory: "one-time",
-                    listOfWhys: [
-                      "Add 1 savings goal",
-                      "Full statistics & charts",
-                      "Color customization",
-                      "Progress tracking",
-                    ],
-                  ),
-                  PlanCard(
-                    planTitle: "Dream Pack",
-                    icon: Icons.whatshot,
-                    func: () {},
-                    buttonText: "Get Dream Pack",
-                    dreamCount: "3",
-                    mainColor: colors.purple,
-                    price: "5.00",
-                    priceCategory: "one-time",
-                    listOfWhys: [
-                      "Add 3 savings goals",
-                      "Full statistics & charts",
-                      "Color customization",
-                      "Progress tracking",
-                      "Priority support",
-                    ],
-                  ),
-                  PlanCard(
-                    planTitle: "Unlimited",
-                    icon: Icons.sunny,
-                    func: () {},
-                    buttonText: "Get Unlimited",
-                    dreamCount: "Unlimited",
-                    mainColor: colors.orange,
-                    price: "29.99",
-                    priceCategory: "lifetime",
-                    listOfWhys: [
-                      "Unlimited savings goals",
-                      "Full statistics & charts",
-                      "Color customization",
-                      "Progress tracking",
-                      "Priority support",
-                      "Early access to new features",
-                      "Lifetime updates",
-                    ],
-                  ),
-                ],
-              ),
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 16.r),
+      sliver: SliverToBoxAdapter(
+        child: Column(
+          spacing: 15.h,
+          children: [
+            PlanCard(
+              planTitle: "Starter",
+              icon: Icons.webhook_outlined,
+              func: () {},
+              buttonText: "Get Starter",
+              dreamCount: "1",
+              mainColor: colors.blue,
+              price: "2.99",
+              priceCategory: "one-time",
+              listOfWhys: [
+                "Add 1 savings goal",
+                "Full statistics & charts",
+                "Color customization",
+                "Progress tracking",
+              ],
             ),
-          );
+            PlanCard(
+              planTitle: "Dream Pack",
+              icon: Icons.whatshot,
+              func: () {},
+              buttonText: "Get Dream Pack",
+              dreamCount: "3",
+              mainColor: colors.purple,
+              price: "5.00",
+              priceCategory: "one-time",
+              listOfWhys: [
+                "Add 3 savings goals",
+                "Full statistics & charts",
+                "Color customization",
+                "Progress tracking",
+                "Priority support",
+              ],
+            ),
+            PlanCard(
+              planTitle: "Unlimited",
+              icon: Icons.sunny,
+              func: () {},
+              buttonText: "Get Unlimited",
+              dreamCount: "Unlimited",
+              mainColor: colors.orange,
+              price: "29.99",
+              priceCategory: "lifetime",
+              listOfWhys: [
+                "Unlimited savings goals",
+                "Full statistics & charts",
+                "Color customization",
+                "Progress tracking",
+                "Priority support",
+                "Early access to new features",
+                "Lifetime updates",
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   SliverPadding _buildCurrentPlanSection(
@@ -121,10 +177,15 @@ class PremiumScreen extends StatelessWidget {
                     height: 50.r,
                     width: 50.r,
                     decoration: BoxDecoration(
-                      color: colors.purple,
+                      color: colors.purple.withValues(alpha: 0.2),
+                      border: Border.all(color: colors.purple, width: 1.5.r),
                       borderRadius: BorderRadius.circular(16.r),
                     ),
-                    child: Icon(Icons.star, color: colors.white, size: 24.r),
+                    child: Icon(
+                      Icons.verified,
+                      color: colors.white,
+                      size: 24.r,
+                    ),
                   ),
                   Column(
                     spacing: 3.h,
@@ -249,3 +310,59 @@ class PremiumScreen extends StatelessWidget {
   }
 }
 
+class WhyUpgradeCard extends StatelessWidget {
+  const WhyUpgradeCard({
+    super.key,
+    required this.icon,
+    required this.mainColor,
+    required this.title,
+    required this.subTitle,
+  });
+  final String title, subTitle;
+  final IconData icon;
+  final Color mainColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppStateWrapper(
+      builder: (colors, texts, colorScheme) => Row(
+        spacing: 10.w,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 35.r,
+            width: 35.r,
+            decoration: BoxDecoration(
+              color: mainColor.withValues(alpha: 0.3),
+              borderRadius: BorderRadius.circular(10.r),
+            ),
+            child: Icon(icon, color: mainColor, size: 20.r),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: AppTextStyles.roboto.medium(
+                    fontSize: 14.sp,
+                    color: colorScheme.primary,
+                  ),
+                ),
+                Text(
+                  subTitle,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: AppTextStyles.roboto.medium(
+                    fontSize: 14.sp,
+                    color: colorScheme.secondary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
